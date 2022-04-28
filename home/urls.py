d25d0e56c38661632e1c79908d3867fe9b8b9164
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-04-28 14:52:30 +0000

    add home app and templates

--- home/urls.py
+++ home/urls.py
@@ -15,9 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("allauth.urls")),
-    path("", include("home.urls")),
-]
+urlpatterns = [path("", views.index, name="home")]
