368852b09262cf4beaf6467d1b44e7005e3b6d68
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-05-02 18:47:10 +0000

    added product app, model and fixtures for real this time

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     "allauth.account",
     "allauth.socialaccount",
     "home",
+    "products",
 ]
 
 MIDDLEWARE = [
@@ -74,7 +75,7 @@ TEMPLATES = [
         "OPTIONS": {
             "context_processors": [
                 "django.template.context_processors.debug",
-                "django.template.context_processors.request",  # required by allauth
+                "django.template.context_processors.request",  # auth
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
