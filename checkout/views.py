ecb150780496b509788152fee16c162909629f0c
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-06-19 20:09:37 +0000

    add stripe payment elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = "checkout/checkout.html"
     context = {
         "order_form": order_form,
+        "stripe_public_key": "pk_test_51LCU5RG6Ft03rGWXpf53Riaglbln5dKE6mEm3LhAkc3dc4fxUBKcsWmNMHB711FcsLeDgExna3uUxyKYGx7Fpune00D2mnBgB1",
+        "client_secret": "test client secret",
     }
 
     return render(request, template, context)
