3268d11fd253ec43d9466b63604ba97574ea1af7
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-07-04 14:17:01 +0000

    deployment update

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -33,11 +33,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://boutique-shop.herokuapp.com/", "localhost"]
 
-CSRF_TRUSTED_ORIGINS = [
-    "https://8000-cmuck2000-ecommerceshop-c1e1yo5a7xj.ws-eu47.gitpod.io/",
-]
 
 # Application definition
 
@@ -127,14 +124,15 @@ WSGI_APPLICATION = "boutique_ado.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if "DATABASE_URL" in os.environ:
+    DATABASES = {"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-
-}
-
 
 
 # Password validation
