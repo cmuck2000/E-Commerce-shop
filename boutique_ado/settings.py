72ea5c92d260088a72305c2bf97c74bbbd0d4962
Cormac Hogan <Cormac_Hogan@protonmail.com>
2022-07-04 19:10:44 +0000

    revert email changes

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -178,10 +178,10 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 if "USE_AWS" in os.environ:
-        # Cache control
+    # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
+        "Expires": "Thu, 31 Dec 2099 20:00:00 GMT",
+        "CacheControl": "max-age=94608000",
     }
 
     # BUCKET CONFIG
@@ -215,14 +215,6 @@ STRIPE_WH_SECRET = os.environ.get("STRIPE_WH_SECRET", "")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# if development:
-#     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-#     DEFAULT_FROM_EMAIL = "boutiqueado@example.com"
-# else:
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    DEFAULT_FROM_EMAIL = "boutiqueado@example.com"
+
